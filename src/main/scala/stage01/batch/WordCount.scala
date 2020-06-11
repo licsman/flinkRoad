b81959389223adea5aabe58586be14211dcd42a8
licsman <licsman@163.com>
2020-06-11 10:06:41 +0800

    flink wordcount

---
+++ src/main/scala/stage01/batch/WordCount.scala
@@ -0,0 +1,18 @@
+package stage01.batch
+
+import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.api.scala._
+
+object WordCount{
+  def main(args: Array[String]): Unit = {
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    val data: DataSet[String] = env.readTextFile("input/text.log")
+    val res: AggregateDataSet[(String, Int)] = data.flatMap(_.split("\\s+").filter(_.nonEmpty))
+      .map((_,1))
+      .groupBy(0)
+      .sum(1)
+    res.writeAsCsv("output/01")
+    env.execute("wordCount")
+  }
+}
